0934355bce8f5c75f3d62293aade29496dab0b22
katsmunoz <katherine.smm@gmail.com>
2019-12-02 14:13:24 -0300

    botones y fondo HTML, CSS sin flexbox

--- src/main.js
+++ src/main.js
@@ -1,15 +1,4 @@
-/**
- * import POKEMON from './data/pokemon/pokemon.js'
- * import LoL from './data/lol/lol.js'
- * import POTTER from './data/potter/potter.js'
- */
 
-import { example } from './data.js';
+import POKEMON from './data/pokemon/pokemon.js'
 
-console.log(example);
-
-/*
- * console.log(POKEMON);
- * console.log(LoL);
- * console.log(POTTER)
-*/
+console.log(POKEMON);
