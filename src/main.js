cdafc29e60f751448dd15f5b3e4ccf25c81142cc
natigrottes <nataly.cuevas.eme@gmail.com>
2019-12-06 22:26:21 -0300

    Prueba de Card y muestra de data en la web

--- src/main.js
+++ src/main.js
@@ -1,39 +1,27 @@
 import POKEMON from './data/pokemon/pokemon.js'
 
-import {pokemonByType} from './data.js'
-console.log(pokemonByType(POKEMON, 'Fire'));
-
-import {pokemonByName} from './data.js'
-console.log(pokemonByName(POKEMON, 'Charmander'));
-
-//console.log(POKEMON);
-
-//console.log(POKEMON[3].name) //muestra nombres según posicion
-
-//console.log(POKEMON[0].type) //muestra los tipos de pokemon segun posicion
-
-//console.log(POKEMON.length) //muestra los 151 pokemon
-
-//let pokemonByType = POKEMON.filter(POKEMON => POKEMON.type);
-//console.log(pokemonByType);
-
-//let pokemonNames = POKEMON.filter(POKEMON => POKEMON.name);
-//console.log(pokemonNames);
-
 //Cambiar de HOME PAGE a SECOND PAGE al hacer click en el boton de la imagen 
 document.getElementById('elegirPokeBtn').addEventListener('click', () => {
     homePage.style.display="none";
     secondPage.style.display="block";
-});
+
+  for (let i = 0; i < POKEMON.length; i++) {
+    // console.log(POKEMON[i].name);
+    const dataPokemon = POKEMON[i];
+    console.log(dataPokemon);
+
+     let card = document.createElement('div');
+     card.classList.add('pokeCard');
+     card.innerHTML +=`<img src = ${POKEMON[i].img} alt ='imgCard'><p${POKEMON[i].num}</p><p>${POKEMON[i].name}</p><p>${POKEMON[i].num}</p>`
+     document.getElementById('container').appendChild(card);
+ 
+}; 
+
 
 //volver al Home apretando HOME
-document.getElementById('pokeHomeBtn').addEventListener('click', limpiar);
+document.getElementById('pokeHomeBtn').addEventListener('click', home);
 
-function limpiar () {
+function home () {
     window.location.reload();    
-}
-
-
-//const allPokemons = document.getElementById('allPokemons');
-//allPokemons.innerHTML = pokemonByType;
+};
 
