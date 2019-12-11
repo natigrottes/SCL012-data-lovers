94118231dc1268e128d9a054d34add294b167df7
natigrottes <nataly.cuevas.eme@gmail.com>
2019-12-11 10:24:02 -0300

    Corrigiendo errores de npm test, 50% avances

--- src/data.js
+++ src/data.js
@@ -1,24 +1,24 @@
 // Filtrar por TIPOS
-export let pokemonByType = (POKEMON, types) => {
+export const pokemonByType = (POKEMON, types) => {
   return POKEMON.filter(pokeTypes => pokeTypes.type.includes(types));
 };
 
 // Ordenar por NOMBRE y NÚMERO
-export let pokemonByName = (POKEMON, sortBy, sortOrder) => {
+export const pokemonByName = (POKEMON, sortBy, sortOrder) => {
   let compare = POKEMON.sort((a,b) => {
     return a[sortBy].localeCompare(b[sortBy]);
   });
 
-if (sortOrder === 'aToZ') {
-  return compare;
-}
-if (sortOrder === 'zToA') {
-  return compare.reverse();
-}
-if (sortOrder === 'menorMayor'){
-  return compare;
-}
-if (sortOrder === 'mayorMenor') {
-  return compare.reverse();
-}
+  if (sortOrder === 'aToZ') {
+    return compare;
+  }
+  if (sortOrder === 'zToA') {
+    return compare.reverse();
+  }
+  if (sortOrder === 'menorMayor') {
+    return compare;
+  }
+  if (sortOrder === 'mayorMenor') {
+    return compare.reverse();
+  }
 };
--- src/main.js
+++ src/main.js
@@ -7,32 +7,31 @@ import { pokemonByType } from './data.js';
 import { pokemonByName } from './data.js';
 
 // Variable del div donde se muestran las cartas
-let pokeBase = document.getElementById('contentOfCards');
+const pokeBase = document.getElementById('contentOfCards');
 
- // Función para crear CARDS por pokemon
+// Función para crear CARDS por pokemon
 function createCards (data) {
-  let pokeCards = document.createElement('button');
+  const pokeCards = document.createElement('button');
   pokeCards.setAttribute('class', 'pokeCardsStyle');
-  // console.log(pokeCards);
-  let pokePic = document.createElement('img');
+// console.log(pokeCards);
+  const pokePic = document.createElement('img');
   pokePic.setAttribute('class', 'pokeCardsPic');
-  let pokeImagen = data.img;
+  const pokeImagen = data.img;
   pokePic.setAttribute('src', pokeImagen);
   pokeCards.appendChild(pokePic);
-  let pokeName = document.createElement('p');
+  const pokeName = document.createElement('p');
   pokeName.setAttribute('class', 'pokeCardsName');
-  let pokemonName = document.createTextNode(data.name);
+  const pokemonName = document.createTextNode(data.name);
   pokeName.appendChild(pokemonName);
   pokeCards.appendChild(pokeName);
-  let pokeLine = document.createElement('hr');
+  const pokeLine = document.createElement('hr');
   pokeLine.setAttribute('class', 'pokeCardsLine');
   pokeCards.appendChild(pokeLine);
-  let pokeNum = document.createElement('p');
+  const pokeNum = document.createElement('p');
   pokeNum.setAttribute('class', 'pokeCardsNum');
-  let pokeNumber = document.createTextNode(data.num);
+  const pokeNumber = document.createTextNode(data.num);
   pokeNum.appendChild(pokeNumber);
   pokeCards.appendChild(pokeNum);
-
   pokeBase.appendChild(pokeCards);
 }
 
@@ -42,25 +41,24 @@ for (let i = 0; i < POKEMON.length; i++) {
 };
 
 // Filtrar por TIPO
-let pokeSelect = document.getElementById('select1');
+const pokeSelect = document.getElementById('select1');
 console.log(pokeSelect);
 pokeSelect.addEventListener('change', filterPokes);
 
 function filterPokes () {
-  let finalFilter = pokemonByType(POKEMON, pokeSelect.value);
+  const finalFilter = pokemonByType(POKEMON, pokeSelect.value);
   document.getElementById('contentOfCards').innerHTML = "";
   for (let i = 0; i < finalFilter.length; i++){
     createCards(finalFilter[i]);
-
   } 
 };
 
 // Ordenar por NOMBRE
-let pokeNames = document.getElementById ('select2');
+const pokeNames = document.getElementById ('select2');
 pokeNames.addEventListener ('change', showSelect2);
 function showSelect2 (){
-  let pokeValue = document.getElementById ('select2').value;
-  let finalSortName = pokemonByName(POKEMON, 'name', pokeValue);
+  const pokeValue = document.getElementById ('select2').value;
+  const finalSortName = pokemonByName(POKEMON, 'name', pokeValue);
   document.getElementById('contentOfCards').innerHTML = '';
   for (let i = 0; i < finalSortName.length; i++) {
     createCards(finalSortName[i]);
@@ -68,17 +66,16 @@ function showSelect2 (){
 };
 
 // Ordenar por NOMBRE
-let pokeNumbers = document.getElementById ('select3');
+const pokeNumbers = document.getElementById ('select3');
 pokeNumbers.addEventListener ('change', showSelect3);
 function showSelect3 (){
-  let pokeValueNum = document.getElementById ('select3').value;
-  let finalSortNum = pokemonByName(POKEMON, 'num', pokeValueNum);
+  const pokeValueNum = document.getElementById ('select3').value;
+  const finalSortNum = pokemonByName(POKEMON, 'num', pokeValueNum);
   document.getElementById('contentOfCards').innerHTML = '';
   for (let i = 0; i < finalSortNum.length; i++) {
     createCards(finalSortNum[i]);
   }
 };
-
 
 //Cambiar de HOME PAGE a SECOND PAGE al hacer click en el boton de la imagen 
 document.getElementById('elegirPokeBtn').addEventListener('click', () => {
@@ -86,11 +83,8 @@ document.getElementById('elegirPokeBtn').addEventListener('click', () => {
   secondPage.style.display = "block";
 });
 
-
-//volver al Home apretando HOME
+// // volver al Home apretando HOME
 document.getElementById('pokeHomeBtn').addEventListener('click', home);
-
-
 function home() {
   window.location.reload();
 };
